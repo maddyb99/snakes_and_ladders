28b9dd4830c6659c42e7b677bf922f55fd25ec5b
maddyb99 <43785086+maddyb99@users.noreply.github.com>
2020-05-06 15:44:59 +0530

    Original Filename:SNAKE20.CPP Last Modified:Sat May 17 01:44:02 2014

--- snake_and_ladder.cpp
+++ snake_and_ladder.cpp
@@ -7,8 +7,8 @@
 #include<string.h>
 class player
 {
-	int position, lastroll,ladder;
-	char plname[30];
+	int position, lastroll,lad;
+	char plname[8],temp[3];
 /*	player()
 	{	position=0;
 	}*/
@@ -22,37 +22,93 @@ class player
 		else
 		{
 			position+=roll;
-			ladder=checkladder(position);
-			if(ladder!=0)
+			lad=checkladder(position);
+			if(lad!=0)
 			{
-				switch(ladder)
-				{
-					case 1:position=ladder1[1];
-						break;
-					case 2:position=ladder2[1];
-						break;
-					case 3:position=ladder3[1];
-						break;
-				}
+				position=ladder[lad-1][1];
 			}
 			lastroll=roll;
 			return(1);
 		}
 	}
 	int getpos(){return(position);}
-	void disp(int x, int y)
+	void disp(int x,int y)
 	{
-		if(ladder==0)
-		{	gotoxy(x+6,y);
-			cout<<"You got a "<<lastroll<<".";
+		if(lad==0)
+		{
+			temp[2]=NULL;
+			temp[0]=position/10+48;
+			temp[1]=position%10+48;
+			if(x!=640)
+			{
+				if(position<100)
+				{
+					outtextxy(x,y+24,"You are now at ");
+					outtextxy(x+textwidth("You are now at "),y+24,temp);
+				}
+				else
+					outtextxy(x,y+24,"You are now at 100!");
+				outtextxy(x,y+12,"You got a ");
+				temp[0]=lastroll+48;
+				temp[1]=NULL;
+				outtextxy(x+textwidth("You got a "),y+12,temp);
+			}
+			else
+			{
+				if(position<100)
+				{
+					outtextxy(x-textwidth("You are now at ")-16,y+24,"You are now at ");
+					outtextxy(x-textwidth(temp),y+24,temp);
+				}
+				else
+					outtextxy(x-textwidth("You are now at 100!"),y+24,"You are now at 100!");
+				outtextxy(x-textwidth("You got a")-16,y+12,"You got a ");
+				temp[0]=lastroll+48;
+				temp[1]=NULL;
+				outtextxy(x-textwidth(temp),y+12,temp);
+			}
 		}
 		else
 		{
-			gotoxy(x,y);
-			cout<<"You got a "<<lastroll<<" and ladder "<<ladder;
+			temp[2]=NULL;
+			temp[0]=position/10+48;
+			temp[1]=position%10+48;
+			if(x!=640)
+			{
+				if(position<100)
+				{
+					outtextxy(x,y+24,"You are now at ");
+					outtextxy(x+textwidth("You are now at "),y+24,temp);
+				}
+				else
+					outtextxy(x,y+24,"You are now at 100!");
+				outtextxy(x,y+12,"You got a ");
+				temp[0]=lastroll+48;
+				temp[1]=NULL;
+				outtextxy(x+textwidth("You got a "),y+12,temp);outtextxy(x+textwidth("You got a "),y+12,temp);
+				outtextxy(x+textwidth("You got a   "),y+12,"and Ladder");
+				temp[0]=lad+48;
+				outtextxy(x+textwidth("You got a   and Laadder "),y+12,temp);
+			}
+			else
+			{
+				if(position<100)
+				{
+					outtextxy(x-textwidth("You are now at ")-16,y+24,"You are now at ");
+					outtextxy(x-textwidth(temp),y+24,temp);
+				}
+				else
+					outtextxy(x-textwidth("You are now at 100!"),y+24,"You are now at 100!");
+				outtextxy(x-textwidth("You got a   and Ladder  "),y+12,"You got a ");
+				temp[0]=lastroll+48;
+				temp[1]=NULL;
+				outtextxy(x-textwidth(temp)-textwidth(" and Ladder  "),y+12,temp);
+				outtextxy(x-textwidth("and Ladder  "),y+12,"and Ladder");
+				temp[0]=lad+48;
+				outtextxy(x-textwidth(temp),y+12,temp);
+			}
+
 		}
-		gotoxy(x+3,y+1);
-		cout<<"You are now at "<<position<<".";
 	}
 	void indetails()
 	{    gets(plname);
@@ -63,72 +119,55 @@ class player
 
 };
 player p[2];
-int ladder1[2],ladder3[2],ladder2[2],snake1[2],snake2[2];
+int ladder[3][2];
 int checkladder(int pos)
 {
-	if(pos==ladder1[0])
-		return(1);
-	else if(pos==ladder2[0])
-		return(2);
-	else if(pos==ladder3[0])
-		return(3);
-	else
-		return(0);
-}
-int snakeladder(int mode)
-{
-	int i,y,x;
-	if (mode==1)
+	int i,j=0;
+	for(i=0;i<3;i++)
 	{
-		do{
-			ladder1[0]=random(80);
-			ladder1[1]=random(100);
-		}while(ladder1[0]<0||ladder1[1]<=ladder1[0]);
-		do{
-			do{
-			ladder2[0]=random(80);
-			ladder2[1]=random(100);
-			}while(ladder2[0]<ladder1[0]);
-		}while(ladder2[0]<0||ladder2[1]<=ladder2[0]);
-		do{
-			do{
-			ladder3[0]=random(80);
-			ladder3[1]=random(100);
-			}while(ladder3[0]<ladder2[0]);
-		}while(ladder3[0]<0||ladder3[1]<=ladder3[0]);
+		if(pos==ladder[i][0])
+			j=i+1;
 	}
-	if(mode==1||mode==0)
+	return(j);
+}
+void snakeladder()
+{
+	int i,x=2,y=7;
+	for(i=0;i<3;i++){
+	if(i==0){
+	do{
+		ladder[i][0]=random(80);
+		ladder[i][1]=random(100);
+	}while(ladder[i][0]<0||ladder[i][1]<=ladder[i][0]+(10-ladder[i][0]%10));
+	}
+	else{
+	do{
+		do{
+		ladder[i][0]=random(80);
+		ladder[i][1]=random(100);
+		}while(ladder[i][0]<=ladder[i-1][0]||ladder[i][1]==ladder[i-1][1]);
+	}while(ladder[i][1]<=ladder[i][0]+(10-ladder[i][0]%10));
+	}}
+
+	for(i=0;i<3;i++,y++)
 	{
-		y=wherey();x=wherex();
-		gotoxy(38,17);
-		cout<<"Ladder:";
-		gotoxy(38,wherey()+1);
-		cout<<"1)"<<ladder1[0]<<"-"<<ladder1[1];
-		gotoxy(38,wherey()+1);
-		cout<<"2)"<<ladder2[0]<<"-"<<ladder2[1];
-		gotoxy(38,wherey()+1);
-		cout<<"3)"<<ladder3[0]<<"-"<<ladder3[1];
 		gotoxy(x,y);
-	}return(0);
+		cout<<i+1<<") "<<ladder[i][0]<<"-"<<ladder[i][1];
+	}
 }
 void gamedisp()
 {
 	clrscr();
 	cleardevice();
-	int x=160,y=40,xi=32,yi=32,*poly,style=SOLID_FILL,i;
+	int x=160,y=40,xi=32,yi=32,*poly,i;
 	char ch[3];
-	outtextxy(8,8,"Currently Winning: ");
-	outtextxy(3,45,"Ladders are at:");
-	outtextxy(515,45,"Snakes are at:");
-	outtextxy(5,350,p[0].getname());
-	int temp=strlen(p[1].getname());
-	outtextxy(640-(strlen(p[1].getname())*9),350,p[1].getname());
 	line(0,20,640,20);
 	line(130,20,130,390);
 	line(510,20,510,390);
 	line(130,390,510,390);
 	line(0,340,130,340);
 	line(510,340,640,340);
+	line(320,390,320,480);
 	poly=new int[10];
 	poly[0]=x-10;
 	poly[1]=y-10;
@@ -140,7 +179,7 @@ void gamedisp()
 	poly[7]=poly[5];
 	poly[8]=poly[0];
 	poly[9]=poly[1];
-	setfillstyle(style,0);
+	setfillstyle(SOLID_FILL,6);
 	fillpoly(5,poly);
 	poly[0]=x;
 	poly[1]=y;
@@ -152,16 +191,17 @@ void gamedisp()
 	poly[7]=poly[5];
 	poly[8]=poly[0];
 	poly[9]=poly[1];
-	setfillstyle(style,1);
+	setfillstyle(SOLID_FILL,11);
+	setcolor(5);
 	fillpoly(5,poly);
 	delete poly;
-	for(i=0;i<11;i++)
+	for(i=1;i<10;i++)
 	{	line(x+(32*i),y,x+(32*i),y+(32*10));
 		line(x,y+(32*i),x+(32*10),y+(32*i));
-		delay(40);
 	}
 	ch[2]=NULL;
 	x=170;y=340;
+	setcolor(getbkcolor());
 	for(i=1;i<101;i++)
 	{
 		if(i==100)
@@ -171,7 +211,6 @@ void gamedisp()
 			ch[1]=i%10+48;
 			outtextxy(x,y,ch);
 		}
-		//cout<<i;
 		if(i%10==0)
 		{       x+=xi;
 			xi=xi*-1;
@@ -180,102 +219,130 @@ void gamedisp()
 		x+=xi;
 		delay(50);
 	}
-
+	setcolor(YELLOW);
+       //	settextstyle(SANS_SERIF_FONT,HORIZ_DIR,1);
+	outtextxy(5,8,"Currently Winning: ");
+	outtextxy(3,75,"Ladders are at:");
+	outtextxy(3,175,"Snakes are at:");
+	settextstyle(DEFAULT_FONT,HORIZ_DIR,2);
+	outtextxy(5,350,p[0].getname());
+	outtextxy(640-textwidth(p[1].getname()),350,p[1].getname());
+	snakeladder();
+	settextstyle(DEFAULT_FONT,HORIZ_DIR,1);
 }
-void endgame(int mode=0)
+void endgame(int mode,int pl)
 {
 	clrscr();
+	cleardevice();
 	switch(mode)
 	{
-		case 0: cout<<"Player ENDED the Game.";
+		case 1: cout<<p[pl].getname()<<" ended the game.";
 			break;
-		default: cout<<"GAME ENDED\n"<<player[mode].getname()<<" WON.";
+		case 0: cout<<"GAME ENDED\n"<<p[pl].getname()<<" WON.";
 			break;
 	}
 	cout<<"\n\nPress any key to exit.";
 }
 void gameplay()
 {
-	char ans,ans2='y';
-	int x[2],check,y,i,pos[2]={0,0},pn;
-	x[0]=8;x[1]=49;
-	for(i=0;i>=0;i++){
-	pn=i%2;
-	if(pn==0)
+	char ans[2],ans2='y',*diecheck="Shall we roll the Die? ",*exitcheck="DO YOU REALLY WANT TO EXIT? ";
+	ans[1]=NULL;
+	int i,pn,y=400,*poly,x[2]={5,640};
+	setfillstyle(SOLID_FILL,0);
+	poly=new int[10];
+	for(i=0;i>=0;i++)
 	{
-		if(wherey()+1>23)
-		{	gamedisp();
-			p[pn].disp(x[pn],wherey()+2);
-			p[1].disp(x[1],wherey()-1);
-		}
-		y=wherey();
-		if(pos[0]>pos[1])
+		pn=i%2;
+		setcolor(getbkcolor());
+		if(pn)
 		{
-			gotoxy(65,1);
-			cout<<" ";
-			gotoxy(24,1);
-			cout<<"*";
+			poly[0]=321;
+			poly[4]=640;
 		}
-		else if(pos[0]<pos[1])
+		else
 		{
-			if(pos[1]>=100)
-			{
-				gotoxy(wherex()-4,wherey());
-				cout<<"100!";
-				delay(1000);
-				endgame(2);
+			poly[0]=319;
+			poly[4]=0;
+		}
+		poly[1]=391;
+		poly[2]=poly[0];
+		poly[3]=480;
+		poly[5]=poly[3];
+		poly[6]=poly[4];
+		poly[7]=poly[1];
+		poly[8]=poly[0];
+		poly[9]=poly[1];
+		fillpoly(5,poly);
+		setcolor(15);
+		if(pn)
+		{	outtextxy(x[pn]-textwidth(diecheck)-textwidth("Y"),y,diecheck);
+			ans[0]=getch();
+			outtextxy(x[pn]-textwidth(ans),y,ans);
+		}
+		else
+		{	outtextxy(x[pn],y,diecheck);
+			ans[0]=getch();
+			outtextxy(x[pn]+1+textwidth(diecheck),y,ans);
+		}
+		if(ans[0]=='y'||ans[0]=='Y')
+		{
+			while(p[pn].update(random(7))==0) ;
+			p[pn].disp(x[pn],y);
+		}
+		else
+		{
+			if(pn)
+				outtextxy(640-textwidth(exitcheck)-textwidth("Y"),y+12,exitcheck);
+			else
+				outtextxy(5,y+12,exitcheck);
+			ans2=getch();
+			if(ans2=='y'||ans2=='Y')
+			{	endgame(1,pn);
 				break;
 			}
-			gotoxy(24,1);
-			cout<<" ";
-			gotoxy(65,1);
-			cout<<"*";
+			else
+				i--;
 		}
-		gotoxy(x[pn],y+2);
-	}
-	else if(i%2!=0)
-	{
-
-		if(pos[0]>=100)
-		{
-			gotoxy(wherex()-4,wherey());
-			cout<<"100!";
-			delay(1000);
-			endgame(1);
-			break;
+		setcolor(getbkcolor());
+		poly[0]=5+textwidth("Currently winning: ");
+		poly[1]=0;
+		poly[2]=poly[0];
+		poly[3]=19;
+		poly[4]=poly[0]+textwidth("ABCDEFGH");
+		poly[5]=poly[3];
+		poly[6]=poly[4];
+		poly[7]=poly[1];
+		poly[8]=poly[0];
+		poly[9]=poly[1];
+		fillpoly(5,poly);
+		setcolor(15);
+		if(p[0].getpos()>p[1].getpos())
+		{	if(p[0].getpos()>=100)
+			{	endgame(0,0);
+				break;
+			}
+			else
+				outtextxy(5+textwidth("Currently Winning: "),8,p[0].getname());
 		}
-		gotoxy(x[pn],wherey()-2);
-	}
-	cout<<"Shall we roll the dice?";
-	ans=getch();
-	cout<<ans<<"\n";
-	if(ans=='y'||ans=='Y')
-	{
-		do{
-		check=p[pn].update(random(7));
-		}while(check==0);
-		p[pn].disp(x[pn],wherey());
-	}
-	else
-	{
-		gotoxy(x[pn]+1,wherey());
-		cout<<"Do you want to exit?";
-		ans2=getch();
-		cout<<ans2<<"\n";
-		if(ans2=='y'||ans2=='Y')
-		{
-			endgame();
-			break;
+		else if (p[0].getpos()<p[1].getpos())
+		{	if(p[1].getpos()>=100)
+			{	endgame(0,1);
+				break;
+			}
+			else
+				outtextxy(5+textwidth("Currently Winning: "),8,p[1].getname());
 		}
+		delay(250);
 	}
-	pos[0]=p[0].getpos();
-	pos[1]=p[1].getpos();     // delay(1000);
-	}
+	delete poly;
+	delete diecheck;
+	delete exitcheck;
 }
 
 
 void main()
 {
+	clrscr();
 	int gdriver=VGA,gmode=VGAHI;
 	initgraph(&gdriver,&gmode,"");
 	randomize();
@@ -283,8 +350,10 @@ void main()
 	p[0].indetails();
 	cout<<"Enter Name of Player 2: ";
 	p[1].indetails();
+	//setbkcolor(15);
 	gamedisp();
+	gameplay();
 	getch();
-	endgame(0);
+	closegraph();
 	exit(0);
 }
